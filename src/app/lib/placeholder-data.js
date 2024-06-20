5d659c26eab85ed1dd480ea5bfab979aafd26bba
eddynice <osareniyeosazee@gmail.com>
2024-06-20 09:40:42 +0100

    my nextjs app

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,15 @@
+const invoice = [
+{
+    customer_id: customers[0].id,
+    amount: 23212,
+    status: 'pending',
+    data: '2022-12-06',
+},
+{
+    customer_id: customers[0].id,
+    amount: 13212,
+    status: 'pending',
+    data: '2022-12-14',
+}
+
+]
